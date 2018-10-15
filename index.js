9ea24954b26d9d53bcd1f8b722a6673e60c988f9
Frank <xuzy213@gmail.com>
2018-10-15 17:28:56 +1300

    init dependencies

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
-import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import { makeExecutableSchema } from 'graphql-tools';
 import typeDefs from './schema';
-import resolvers from './resolvers'
+import resolvers from './resolvers';
 
 const schema = makeExecutableSchema({
   typeDefs,
@@ -13,14 +12,9 @@ const schema = makeExecutableSchema({
 const PORT = 5000;
 const app = express();
 
-const graphqlEndPoint = 'graphql'
-
 app.use('/graphql', graphqlHTTP({
   schema,
-  graphiql: true
-}))
+  graphiql: true,
+}));
 
-// app.use(graphqlEndPoint, bodyParser.json(), graphqlExpress({ schema }));
-// app.use('/graphiql', graphiqlExpress({ endpointURL: graphqlEndPoint }));
-
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
