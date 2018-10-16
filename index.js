f7057fc1820e17f3aaa3581ecb6a4c6549dcc76a
Frank <xuzy213@gmail.com>
2018-10-16 15:32:06 +1300

    refactor schema

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+import path from 'path';
+import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import { makeExecutableSchema } from 'graphql-tools';
-import typeDefs from './schema';
-import resolvers from './resolvers';
+// import typeDefs from './schema';
+// import resolvers from './resolvers';
 import models from './models';
+// ./graphql/typeDefs.js
 
+const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')), { all: true });
+const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers')), { all: true });
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers,
