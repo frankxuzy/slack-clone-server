cfca09dbda5c6191914c5cbc242e2367c247ad99
Frank <xuzy213@gmail.com>
2018-10-17 13:34:34 +1300

    add cors to allow cross-origin requests

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import { makeExecutableSchema } from 'graphql-tools';
-// import typeDefs from './schema';
-// import resolvers from './resolvers';
+import cors from 'cors';
 import models from './models';
-// ./graphql/typeDefs.js
+
 
 const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')), { all: true });
 const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers')), { all: true });
@@ -17,6 +16,9 @@ const schema = makeExecutableSchema({
 
 const PORT = 5000;
 const app = express();
+
+// allow cross-origin requests
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
   schema,
