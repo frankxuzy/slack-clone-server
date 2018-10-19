3ed68ba25f9a3dd1e79694441075e66ffcdb6453
Frank <xuzy213@gmail.com>
2018-10-19 23:20:01 +1300

    add addUser decode token and get user id

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import { makeExecutableSchema } from 'graphql-tools';
 import cors from 'cors';
+import jwt from 'jsonwebtoken';
+
 import models from './models';
+import { refreshTokens } from './utils/auth';
 
 const SECRET = '98q34riunskjvb9e7wiqbdlksandf';
 const SECRET2 = 'hqwkrj2983isubdf4893qkjeefowqrifhc90u4q5';
@@ -18,10 +21,31 @@ const schema = makeExecutableSchema({
 const PORT = 5000;
 const app = express();
 
+const addUser = async (req, res, next) => {
+  const token = req.headers['x-token'];
+  if (token) {
+    try {
+      const { user } = jwt.verify(token, SECRET);
+      req.user = user;
+    } catch (err) {
+      const refreshToken = req.headers['x-refresh-token'];
+      const newTokens = await refreshTokens(token, refreshToken, models, SECRET, SECRET2);
+      if (newTokens.token && newTokens.refreshToken) {
+        res.set('Access-Control-Expose-Headers', 'x-token, x-refresh-token');
+        res.set('x-token', newTokens.token);
+        res.set('x-refresh-token', newTokens.refreshToken);
+      }
+      req.user = newTokens.user;
+    }
+  }
+  next();
+};
+
+app.use(addUser);
 // allow cross-origin requests
 app.use(cors());
 
-app.use('/graphql', graphqlHTTP({
+app.use('/graphql', graphqlHTTP(req => ({
   schema,
   graphiql: true,
   context: {
@@ -29,11 +53,9 @@ app.use('/graphql', graphqlHTTP({
     SECRET,
     SECRET2,
     // only bind owner to user 1 for now
-    user: {
-      id: 1,
-    },
+    user: req.user,
   },
-}));
+})));
 
 // force: true drop all tables before start then redo it
 // models.sequelize.sync({ force: true }).then(() => {
